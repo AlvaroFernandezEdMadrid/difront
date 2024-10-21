e85aa628b504c1affb73a6fd277af56646702ad3
Alvaro Fernandez Lopez <alvaro.fernandez223@educa.madrid.org>
2024-10-21 20:15:30 +0200

    lunes

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Grid() {
 return(
-    <div class="grid grid-cols-2">
-        <div class=""></div>
-        <div class=""></div>
+    <div class="grid grid-rows-2 lg:grid-cols-2 h-screen font">
+        <div class="rounded-md bg-green-400 shadow-md w-64">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLgDd9upJwVMgYFCkYZie0yIvuScP2XbeVig&s" alt="cobble" />
+        </div>
+        <div class="rounded-md bg-green-400 shadow-md w-64">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdkDivs-WxplKe2PYuS8qvajg9kJssQnHhjA&s" alt="cobble" />
+        </div>
     </div>
 )
 }
